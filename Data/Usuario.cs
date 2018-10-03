bb7de08cbd57e7f763d7e6fcfb761745f44a2b9d
Joab Rodrigues <joabrodriguesfilho@gmail.com>
2018-10-02 22:02:19 -0300

    * Usuario.cs: pagina de get de usuário.
    * FirstProject.csproj:
    * Index.cshtml:
    * Index.cshtml.cs:
    * Cadastrar.cshtml:
    * Cadastrar.cshtml.cs:

---
+++ Data/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FirstProject.Data
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
---
+++ Pages/Usuarios/Cadastrar.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstProject.Pages.Usuarios
+{
+    public class CadastrarModel : PageModel
+    {
+        public Usuario usuario;
+
+        public void OnGet(string nome, string senha)
+        {
+            usuario = new Usuario
+            {
+                Nome = nome,
+                Senha = senha
+            };
+        }
+    }
+}
